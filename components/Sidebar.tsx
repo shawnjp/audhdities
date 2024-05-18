04630408dd1c2a626568c20f93d57d83a50467d2
shawnjp <34354175+shawnjp@users.noreply.github.com>
2024-05-17 23:46:47 -0400

    cheeky

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import { UserButton } from '@clerk/nextjs';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to manage sidebar visibility
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <div className="sidebar w-64 h-full shadow-md bg-white absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
-      <div className="sidebar-header p-4 flex justify-between items-center">
-        <h1 className="text-lg font-bold">Menu</h1>
-      </div>
-      <div className="user-button-large">
+      <div className="sidebar-header p-14 flex justify-between items-center">
+        
       <UserButton />
+   
       </div>
+ 
       <ul className="sidebar-links">
         <li className="p-4 hover:bg-gray-100">
-          <Link href="/">
+          <Link href="transform: translateX(0)/">
             Home
           </Link>
         </li>
