a84a19dbd1dfb89a7288de2176153447cb063ceb
Shawnjp <shawnjp@gmail.com>
2024-05-20 12:59:11 -0400

    checkin fix menu button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import Sidebar from './Sidebar';
+import MenuButton from './MenuButton';
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,6 +11,7 @@ type LayoutProps = {
 const Layout: React.FC<LayoutProps> = ({ children, isOpen, toggleSidebar }) => {
   return (
     <div className="flex">
+      <MenuButton toggleSidebar={toggleSidebar} /> 
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       <div className="content flex-1 p-4">
         {children}
