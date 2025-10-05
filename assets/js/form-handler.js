941c06d365146398df377a8486e3e857faddd05f
KP <34354175+shawnjp@users.noreply.github.com>
2025-10-04 21:02:06 -0500

    Update form-handler.js

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -118,7 +118,7 @@ class SovereignFormHandler {
         this.showNotification('🔄 Resource added to sovereign pool! Network connecting...', 'success');
         form.querySelector('input[type="text"]').value = '';
     }
-
+    
     handlePaymentInitiation(paymentType) {
         const submission = {
             type: 'payment_initiation',
@@ -189,6 +189,16 @@ class SovereignFormHandler {
         console.log('💾 Submissions export ready:', url);
     }
 
+    // Add to form-handler.js
+    async function saveToGoogleSheets(submission) {
+        const scriptURL = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec';
+        const response = await fetch(scriptURL, {
+            method: 'POST',
+            body: JSON.stringify(submission)
+        });
+        return response.json();
+    }
+    
     showNotification(message, type = 'info') {
         // Create notification element
         const notification = document.createElement('div');
